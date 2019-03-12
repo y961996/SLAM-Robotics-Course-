34739139bc52fc4381b07b05e082d446a6a5dafa
Deneme <yunus10atahan@gmail.com>
2019-03-12 08:48:03 +0300

    Feature Extraction complete (?)

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Course Links:
   https://www.udacity.com/course/artificial-intelligence-for-robotics--cs373
   
 Papers:
-  
+  https://ipsjcva.springeropen.com/articles/10.1186/s41074-017-0027-2
+  https://www.mrpt.org/List_of_SLAM_algorithms
--- SLAM_geohot/extractor.py
+++ SLAM_geohot/extractor.py
@@ -10,6 +10,22 @@ from skimage.transform import EssentialMatrixTransform
 def add_ones(x):
     return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
 
+
+def extractRt(E):
+    W = np.mat([[0, -1, 0], [1, 0, 0], [0, 0, 1]], dtype=float)
+    U, d, Vt = np.linalg.svd(E)
+    assert np.linalg.det(U) > 0
+    if np.linalg.det(Vt) < 0:
+        Vt *= -1.0
+
+    R = np.dot(np.dot(U, W), Vt)
+    if np.sum(R.diagonal()) < 0:
+        R = np.dot(np.dot(U, W.T), Vt)
+    t = U[:, 2]
+    Rt = np.concatenate([R, t.reshape(3, 1)], axis=1)
+    return Rt
+
+
 class Extractor(object):
     def __init__(self, K):
         self.orb = cv2.ORB_create()
@@ -41,6 +57,7 @@ class Extractor(object):
                     ret.append((kp1, kp2))
 
         # filter
+        Rt = None
         if len(ret) > 0:
             ret = np.array(ret)
 
@@ -49,11 +66,15 @@ class Extractor(object):
             ret[:, 1, :] = self.normalize(ret[:, 1, :])
 
             model, inliers = ransac((ret[:, 0], ret[:, 1]),
-                                     FundamentalMatrixTransform,
-                                     min_samples=8, residual_threshold=1,
-                                     max_trials=100)
+                                    #FundamentalMatrixTransform,
+                                    EssentialMatrixTransform,
+                                    min_samples=8,
+                                    #residual_threshold=1,
+                                    residual_threshold=0.005,
+                                    max_trials=200)
+            #print(sum(inliers), len(inliers))
             ret = ret[inliers]
-            #s, v, d = np.linalg.svd(model.params)
+            Rt = extractRt(model.params)
 
         self.last = {'kps': kps, 'des': des}
-        return ret
+        return ret, Rt
